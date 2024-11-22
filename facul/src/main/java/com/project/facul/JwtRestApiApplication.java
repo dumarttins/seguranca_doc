1377b167e8df561b55db5d075b2f473db76381d6
Eduardo Medeiros Martins <91488020+dumarttins@users.noreply.github.com>
2024-11-22 20:44:06 -0300

    Add files via upload

---
+++ facul/src/main/java/com/project/facul/JwtRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.project.facul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRestApiApplication.class, args);
+    }
+}
---
+++ facul/src/test/java/com/project/facul/FaculApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.facul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaculApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
